190e4fbbadc2937ac1f39207bde90ba3a63637df
lajawanti <lajawanti.dhake@gmail.com>
2020-01-14 21:36:54 -0500

    routes and body-parser

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server')
 
-const port = 5000;
+const port = 5000 || process.env.PORT;
 
 app.listen(port, () => {
     console.log(`App running on port : ${port}`)
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 
 const server = express();
+const bodyParser = require("body-parser");
+
+const userRoutes = require('./api/routes/users');
+
+// Configure body-parser settings//
+// urlencoded is for bodies that have UTF-8 encoding.
+// If {extended: false} you cannot use nested objects.
+// e.g. nested obj = {person:{name: adam}}
+server.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse json with body-parser
+server.use(bodyParser.json());
+
+
 
 server.get('/', (req, res) => {
     res.send("Server ip 👍 💡 🎃 ")
