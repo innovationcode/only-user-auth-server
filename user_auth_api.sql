6d5eb72a79a0732cfe019dd6125ffe6776e1c3fd
lajawanti <lajawanti.dhake@gmail.com>
2020-01-14 22:10:50 -0500

    sql query added

---
+++ user_auth_api.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS user_auth_api;
+CREATE DATABASE user_auth_api;
+
+\C user_auth_api;
+
+CREATE EXTENSION IF NOT EXISTS "UUID-OSSP";
+
+CREATE TABLE users(
+    id uuid UNIQUE DEFAULT uuid_generate_v4(),
+    email VARSCHAR(128) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
+    registered BIGINT,
+    token VARCHAR(128) UNIQUE,
+    createdtime BIGINT,
+    emailVerified BOOLEAN,
+    tokenusedbefore BOOLEAN,
+    PRIMARY KEY (email)
+);
