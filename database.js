182742283257284e90dd48c36f3c8a24ea41c564
lajawanti <lajawanti.dhake@gmail.com>
2020-01-15 14:31:05 -0500

    database and users table

---
+++ database.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+require('dotenv').config();
+console.log(process.env.DATABASE_PASSWRD);
+
+const database = knex({
+    client : "pg", //pg is the database library for postgreSQL on knexjs
+    connection : {
+        host: "127.0.0.1", // Your local host IP
+        user : "postgres", // Your postgres user name
+        password: process.env.DATABASE_PASSWRD, // Your postgres user password
+        database: "user_auth_api", // Your database name
+    }
+});
+
+module.exports = database;
--- user_auth_api.sql
+++ user_auth_api.sql
@@ -1,13 +1,12 @@
 DROP DATABASE IF EXISTS user_auth_api;
 CREATE DATABASE user_auth_api;
-
-\C user_auth_api;
+\c user_auth_api;
 
 CREATE EXTENSION IF NOT EXISTS "UUID-OSSP";
 
 CREATE TABLE users(
     id uuid UNIQUE DEFAULT uuid_generate_v4(),
-    email VARSCHAR(128) NOT NULL UNIQUE,
+    email VARCHAR(128) NOT NULL UNIQUE,
     password VARCHAR(128) NOT NULL,
     registered BIGINT,
     token VARCHAR(128) UNIQUE,
