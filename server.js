0ea5cfad4d5a85cc7e45388917594e6a762b7b35
lajawanti <lajawanti.dhake@gmail.com>
2020-01-14 21:41:52 -0500

    users.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ server.use(bodyParser.urlencoded({ extended: true }));
 // Parse json with body-parser
 server.use(bodyParser.json());
 
-
+// Setup your api routes with express
+server.use("/api/users", userRoutes);
 
 server.get('/', (req, res) => {
     res.send("Server ip 👍 💡 🎃 ")
