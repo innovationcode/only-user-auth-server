c5c413a6cdb442bbc19dd1b9e6a4819b12faca9f
lajawanti <lajawanti.dhake@gmail.com>
2020-01-17 13:58:20 -0500

    sendEmail function using aws

--- utilities/sendEmail.js
+++ utilities/sendEmail.js
@@ -1,9 +1,42 @@
 const aws = require('aws-sdk');
-require('dotenv').config({ path: '../.env'});
+require('dotenv').config({ path: '../.env'}); //to access root directory .env file set path in config
 
-console.log(process.env.DATABASE_PASSWRD);
-
-console.log(process.env.AWS_ACCESS_KEY_ID)
-console.log(process.env.AWS_SECRET_KEY)
+aws.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+aws.config.secretAccessKey = process.env.AWS_SECRET_KEY
 aws.config.region = "us-east-1"; 
-console.log(aws.config.region)
+
+//create an Email function
+function Email(to, sub, content) {
+    let ses = new aws.SES();
+
+    let from = "user@example.com";  // The email address added here must be verified in Amazon SES
+    
+    //Amazon SES email format
+    ses.sendEmail({
+        Source: from, 
+        Destination: {ToAddresses: to},
+        Message: {
+                  Subject: {
+                            Data: sub
+                  },
+                  Body:{
+                        Html: {
+                                Data: content
+                        }
+                  }
+        },
+        function(err, data) {
+                              if (err) {
+                                        console.log(err);
+                              } else {
+                                       console.log("Email sent:");
+                                       console.log(data);
+                              }
+        }
+    });
+}
+
+// Export the Email function
+module.exports = {
+                   Email
+                 };
